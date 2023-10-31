b34d1fe7014719f9cd7b59cd29902abecdf125ac
Sara Schoonover <Saramschoon@gmail.com>
2023-10-31 16:16:00 +0000

    added places page with working API

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,24 +5,14 @@ require 'json'
 class PlacesController < ApplicationController
   before_action :set_place, only: %i[ show edit update destroy ]
 
+
+  def home
+    render "places/home"
+  end
+
   # GET /places or /places.json
   def index
     @places = Place.all
-
-    url = URI("https://api.yelp.com/v3/businesses/search?location=Nashville%2C%20TN&sort_by=best_match&limit=20")
-
-    api_key = ENV.fetch("YELP_KEY")
-    header = 
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-
-    request = Net::HTTP::Get.new(url)
-    request["accept"] = 'application/json'
-    request["Authorization"] = "Bearer #{api_key}"
-
-    response = http.request(request)
-    actual_response = response.read_body
-    @parsed_data = JSON.parse(actual_response).fetch("businesses")
   end
 
   # GET /places/1 or /places/1.json
@@ -32,6 +22,38 @@ class PlacesController < ApplicationController
   # GET /places/new
   def new
     @place = Place.new
+    
+  end
+
+  # TODO: move to SearchController#index
+  # TODO: move to YelpClient service object eg YelpClient.new.search(query)
+  def query
+    query_string = {
+      location: params.fetch("location"),
+      price: params.fetch("price").to_i,
+      limit: 20,
+      sort_by: "best_match",
+      categories: "restaurants",
+      term: "restaurants"
+    }.to_query
+    pp query_string
+    base_url = "https://api.yelp.com/v3"
+
+    url = URI("#{base_url}/businesses/search?#{query_string}")
+
+    api_key = ENV.fetch("YELP_KEY")
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    
+    request = Net::HTTP::Get.new(url)
+    request["accept"] = 'application/json'
+    request["Authorization"] = "Bearer #{api_key}"
+    
+    response = http.request(request)
+    actual_response = response.read_body
+    @restaurants = JSON.parse(actual_response).fetch("businesses")
+    
+    render "places/results"
   end
 
   # GET /places/1/edit
@@ -41,7 +63,7 @@ class PlacesController < ApplicationController
   # POST /places or /places.json
   def create
     @place = Place.new(place_params)
-
+    pp @place.name
     respond_to do |format|
       if @place.save
         format.html { redirect_to place_url(@place), notice: "Place was successfully created." }
