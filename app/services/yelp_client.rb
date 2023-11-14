e7dbf2ca5cfb10df2a822435ed406f70b5abb24b
Sara Schoonover <Saramschoon@gmail.com>
2023-11-14 22:46:15 +0000

    created random restaurant logic

--- app/services/yelp_client.rb
+++ app/services/yelp_client.rb
@@ -5,6 +5,24 @@ class YelpClient
     @api_key = ENV.fetch("YELP_KEY")
   end
 
+  def search_businesses(params = {})
+  location = params.fetch("location", "Chicago")
+  categories = params.fetch("categories", "restaurants")
+
+  query_string = {
+    location: location,
+    price: "1, 2, 3, 4",
+    limit: 20,
+    sort_by: "best_match",
+    categories: categories,
+    rating: "ratings",
+    review_count: "review-count",
+  }.to_query
+  url = URI("#{BASE_URL}/businesses/search?#{query_string}")
+
+  fetch(url).fetch("businesses") || []
+  end
+
   def random_business
     # TODO: return an array of 1 business
     businesses = search_businesses
@@ -12,24 +30,7 @@ class YelpClient
   end
 
 
-  def search_businesses(params = {})
-    location = params.fetch("location", "Chicago")
-    categories = params.fetch("categories", "restaurants")
-
-    query_string = {
-      location: location,
-      price: "1, 2, 3, 4",
-      limit: 20,
-      sort_by: "best_match",
-      categories: categories,
-      rating: "ratings",
-      review_count: "review-count",
-    }.to_query
-    url = URI("#{BASE_URL}/businesses/search?#{query_string}")
-
-    fetch(url).fetch("businesses")
-  end
-
+ 
   private
 
   def fetch(url)
